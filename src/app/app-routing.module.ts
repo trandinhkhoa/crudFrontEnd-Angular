911290aa72876624785d5e052b4d0a0c739bfce5
Khoa TRAN <khoatrandinh94@gmail.com>
2022-05-28 23:06:48 +0200

    Add Navbar and Router View

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListsComponent } from './components/tutorials-lists/tutorials-lists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'tutorials' },
+  {path: 'tutorials', component: TutorialsListsComponent },
+  {path: 'tutorials/:id', component: TutorialDetailsComponent},
+  {path: 'add', component: AddTutorialComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
